e68763d2df55f3f15bffee360f899cbea3378b71
Blake Mason <blnktd@gmail.com>
2014-09-01 14:07:03 -0700

    CRUD routes for adventure and chapter models

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,17 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  match('adventures', {:via => :get, :to => 'adventures#index'})
+  match('adventures/new', {:via => :get, :to => 'adventures#new'})
+  match('adventures', {:via => :post, :to => 'adventures#create'})
+  match('adventures/:id', {:via => :get, :to => 'adventures#show'})
+  match('adventures/:id/edit', {via: :get, to: 'adventures#edit'})
+  match('adventures/:id', {:via => [:patch, :put], :to => 'adventures#update'})
+  match('adventures/:id', {:via => :delete, :to => 'adventures#destroy'})
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  match('adventures/adventure_id/chapters', {:via => :get, :to => 'chapters#index'})
+  match('adventures/adventure_id/chapters/new', {:via => :get, :to => 'chapters#new'})
+  match('adventures/adventure_id/chapters', {:via => :post, :to => 'chapters#create'})
+  match('adventures/adventure_id/chapters/:id', {:via => :get, :to => 'chapters#show'})
+  match('adventures/adventure_id/chapters/:id/edit', {via: :get, to: 'chapters#edit'})
+  match('adventures/adventure_id/chapters/:id', {:via => [:patch, :put], :to => 'chapters#update'})
+  match('adventures/adventure_id/chapters/:id', {:via => :delete, :to => 'chapters#destroy'})
 end
